57d275787cd44d6c8574880108c5477657c5805a
Pranav_Gupta08 <pranavgupta.kuna08@gmail.com>
2024-10-05 15:26:25 +0530

    update uploading products

--- backend/routes/imageupload.js
+++ backend/routes/imageupload.js
@@ -1,8 +1,29 @@
-const express = require("express");
-const { saveimage } = require('../controllers/saveimageController');
+// uploadRoutes.js
+const express = require('express');
+const multer = require('multer');
+const { uploadImage } = require('../controllers/saveimageController');
+
 const router = express.Router();
 
+// Multer Configuration (Memory storage)
+const storage = multer.memoryStorage();  // Store files in memory
+const upload = multer({ storage });
 
-router.post('/image', saveimage);
+// POST route for image upload
+router.post('/image', upload.single('product'), uploadImage);
 
 module.exports = router;
+
+
+
+
+
+
+// const express = require("express");
+// const { saveimage } = require('../controllers/saveimageController');
+// const router = express.Router();
+
+
+// router.post('/image', saveimage);
+
+// module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 const multer=require("multer");
+const cloudinary = require('cloudinary').v2;
 const cors = require("cors");
 const path = require("path");
 
@@ -17,17 +18,27 @@ app.use(cors());
 app.get('/',(req,res)=>{
   res.status(200).send("Express App is Running cool")
 })
-const storage=multer.diskStorage({
-  destination:'./Upload/images',
-  filename:(req,file,cb)=>{
-      return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-  }
-})
-const upload=multer({storage:storage})
-app.use('/images',express.static('Upload/images'))
+// const storage=multer.diskStorage({
+//   destination:'./Upload/images',
+//   filename:(req,file,cb)=>{
+//       return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+//   }
+// })
+// const upload=multer({storage:storage})
+// app.use('/images',express.static('Upload/images'))
 
-app.use('/upload',upload.single('product'),require('./routes/imageupload'))
+// app.use('/upload',upload.single('product'),require('./routes/imageupload'))
+const uploadRoutes = require('./routes/imageupload');
 
+// Cloudinary configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// ROUTES BELOW
+app.use('/upload', uploadRoutes);
 // Route for  Products
 
 app.use("/api/moviesproducts", require("./routes/moviesRoutes"));
