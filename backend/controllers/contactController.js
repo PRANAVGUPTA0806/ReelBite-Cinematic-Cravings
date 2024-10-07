70af16778ab252bfac79782df411bf255ab8c2f0
Pranav_Gupta08 <pranavgupta.kuna08@gmail.com>
2024-10-07 23:58:24 +0530

    add mail feature in contactus

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,5 +1,18 @@
 const Contact = require('../models/contactModel');
 const User = require('../models/userModel');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+// Create a transporter
+const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,  // SMTP server from .env
+    port: process.env.EMAIL_PORT || 587,  // SMTP port from .env
+    secure: process.env.SMTP_PORT == 465,  // True for port 465, false for other ports
+    auth: {
+        user: process.env.EMAIL_USER,  // Your email address from .env
+        pass: process.env.EMAIL_PASS,  // Your email password from .env
+    },
+});
 
 // @desc    Create a new contact submission
 // @route   POST /api/contact
@@ -29,6 +42,16 @@ const createContactSubmission = async (req, res) => {
 
         // Save the new contact submission
         await newSubmission.save();
+
+        const mailOptions = {
+            from: process.env.EMAIL_USER,  // Sender address from .env
+            to: email,  // Email address where feedback notification should be sent
+            subject: 'New Feedback Submitted',
+            text: `Hello ${fullName},\n\nEmail: ${email}\nThank you for your feedback! We appreciate your input and will get back to you if necessary.\n\nYour feedback: ${message}\n\nBest regards,\nThe NutriiNuts Team`,
+        };
+
+        // Send the email
+        await transporter.sendMail(mailOptions);
 
         // Return a success response
         res.status(201).json({
