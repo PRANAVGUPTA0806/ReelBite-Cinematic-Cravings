1b4c359035fc0fa4f69aa6619b1ccb4e941f8d9d
Pranav_Gupta08 <pranavgupta.kuna08@gmail.com>
2024-10-10 14:13:12 +0530

    update

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import './Navbar.css'
 import { Link, useNavigate } from 'react-router-dom';
 import navlogo from '/pp1.png'
@@ -6,6 +6,25 @@ import navprofile from '../../assets/nav-profile.svg'
 
 const Navbar = () => {
   const navigate = useNavigate();
+  useEffect(() => {
+    const storedToken = localStorage.getItem("auth-token");
+    if (storedToken) {
+      setToken(storedToken);
+      axios
+        .get(`${import.meta.env.VITE_BACKEND_URL}/api/user/profile`, {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        })
+        .then((response) => {
+          localStorage.setItem({
+            'avatar': response.data.imageUrl || "https://res.cloudinary.com/dwprhpk9r/image/upload/v1728546051/uploads/product_1728546048771.png.png",
+          });
+        })
+        .catch((error) => {
+          console.error("Error fetching user details:", error);
+        });
+    }
+  }, []);
+
   return (
     <div className='navbar'>
       <Link to='/home'><img src={navlogo} alt="logo" className="nav-logo" /></Link>
